8acd975892d54e769469bd199595ba63b1ac62ed
Huynh Thien  Y <huynhthieny@Huynhs-Mac-mini.local>
2016-09-06 16:31:23 +0700

    install React

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var browserity = require('browserify');
 var babelity = require('babelify')
 var source = require('vinyl-source-stream');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 gulp.task('default', function(){
     return browserity('./source/app.js')
